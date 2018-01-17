e4e4c0e17ba792997909353979f1221884a68a0f
Cameron Anderson and Kayla Ondracek <kayla@>
2018-01-17 15:38:03 -0800

    use single input and submit button for all functions

--- js/scripts.js
+++ js/scripts.js
@@ -8,23 +8,32 @@ function factorial(max) {
 }
 
 function rFactorial(max) {
+  if (isNaN(max)) {
+    return "You should learn what a number is..."
+  }
   if (max === 1) {
     return 1;
   }
   return max * rFactorial(max - 1);
 }
 
+//palindrome functions
 function wordCheck(word) {
-  word = word.toLowerCase().replace(/ /g, "").replace(/[\W]/g, "");
-  console.log(word);
+  word = word.toLowerCase().replace(/ /g, "").replace(/[\W,]/g, "");
+
   for (var i = 0; i < word.length/2; i++) {
     if (word.charAt(i) !== word.charAt(word.length-(i+1))) {
-      return false;
+      return "try again";
     }
-  } return true;
+  }
+  return "nice palindrome, jerk";
 }
 
+//prime number functions
 function primes(max) {
+  if (isNaN(max)) {
+    return "You should learn what a number is..."
+  }
   var numbers = [];
   for (var i = 2; i <= max; i++) {
     numbers.push(i);
@@ -38,25 +47,44 @@ function primes(max) {
   return numbers;
 }
 
-
 $(document).ready(function() {
+
+  var mode = ""
+
+  $(".form").submit(function(event) {
+    event.preventDefault();
+    var output;
+
+    if (mode === "f") {
+      output = rFactorial(parseInt($("#input-num").val()));
+    } else if (mode === "p") {
+      output = wordCheck($("#input-num").val());
+    } else if (mode === "pr" ){
+      output = primes(parseInt($("#input-num").val()));
+    }
+    $("#output").hide();
+    $("#output").text("Your results are: " + output);
+    $("#output").fadeIn();
+  });
+
+
+
   $("#factorial").click(function() {
-    var max = parseInt(prompt("How high should we go?"));
-    alert(rFactorial(max));
+    $("label").hide();
+    $(".factorial").fadeIn();
+    mode = "f";
   });
 
   $("#palindromes").click(function() {
-    var word = prompt("Enter a palindrome");
-    if (wordCheck(word) === true) {
-      alert("nice palindrome, jerk");
-    } else {
-      alert("try again");
-    }
+    $("label").hide();
+    $(".palindrome").fadeIn();
+    mode = "p";
   });
 
   $("#prime").click(function() {
-    var max = parseInt(prompt("Enter a number..."));
-    alert(primes(max));
+    $("label").hide();
+    $(".prime").fadeIn();
+    mode = "pr";
   });
 
 });
